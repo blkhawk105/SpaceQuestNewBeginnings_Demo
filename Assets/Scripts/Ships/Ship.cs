5423a02fe89ad9a9be612f801fcb6d48487e7011
blkhawk105 <blkhawk105@gmail.com>
2024-02-16 11:37:39 -0800

    Updates Ship base class
    * Adds base level properties to the Ship class

--- Assets/Scripts/Ships/Ship.cs
+++ Assets/Scripts/Ships/Ship.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship : MonoBehaviour
+public abstract class Ship : MonoBehaviour
 {
+    [SerializeField] protected int HullStrength = 0;
+    [SerializeField] protected int ShieldLevel = 0;
+
+    [SerializeField] protected int PrimaryGunPower = 0;
+    [SerializeField] protected int SecondaryGunPower = 0;
+
+    [SerializeField] protected int ShipSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
