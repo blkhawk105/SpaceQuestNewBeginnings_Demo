9675ad1b14756ed65692737e634d082e92af6138
blkhawk105 <blkhawk105@gmail.com>
2024-02-22 10:11:17 -0800

    Adds asteroid movement
    * Asteroid now starts offscreen at a random y position
    * Moves horizontally toward the player
    * Is destroyed once it leaves the screen past the player

--- Assets/Scripts/Obstacles/Asteroid.cs
+++ Assets/Scripts/Obstacles/Asteroid.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class Asteroid : FlyingObstacle
 {
+    /// <summary>
+    /// The edge of the playable screen
+    /// </summary>
+    private Vector2 screenBounds;
+    /// <summary>
+    /// The width of the asteroid object
+    /// </summary>
+    private float asteroidWidth;
+    /// <summary>
+    /// The height of the asteroid object
+    /// </summary>
+    private float asteroidHeight;
+    /// <summary>
+    /// The amount of padding added when calculating where the asteroid moves on screen
+    /// </summary>
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        Speed = 5;
+
+        // Set the visible screen area
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         
+        // Set the player's ship width and height
+        asteroidWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        asteroidHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        
+        
+        
+        float randomY = Random.Range(-screenBounds.y + (asteroidHeight * 2), screenBounds.y - (asteroidHeight * 2));
+        transform.position = new(screenBounds.x + (asteroidWidth * 4), randomY, transform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+
+    /// <summary>
+    /// Move the Asteroid across the screen
+    /// </summary>
+    private void Move()
+    {
+        // Move the player forward and backward
+        transform.Translate(Time.deltaTime * Speed * Vector3.left);
+
+        if(transform.position.x < -screenBounds.x - (asteroidWidth * 4))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/Obstacles/FlyingObstacle.cs
+++ Assets/Scripts/Obstacles/FlyingObstacle.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class FlyingObstacle : Obstacle
 {
+    /// <summary>
+    /// The obstacle's movement speed
+    /// </summary>
+    [SerializeField] protected int Speed = 3;
+    
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
         
-    }
+    // }
 
     // Update is called once per frame
-    void Update()
-    {
+    // void Update()
+    // {
         
-    }
+    // }
 }
