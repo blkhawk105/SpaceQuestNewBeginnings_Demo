ad1e38e0841d0a5d10b1165415e4eb2fba2e594d
blkhawk105 <blkhawk105@gmail.com>
2024-02-22 08:46:51 -0800

    Adds basic obstical properties
    * Obsticals can now be set as destroyable
    * If an obstical can be destryed its damage level can be tracked
    * If its strength is <= 0 the GameObject is destroed

--- Assets/Scripts/Obstacles/Obstacle.cs
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Can this object be destroyed
+    /// </summary>
+    [SerializeField] protected bool IsDestroyable = true;
+
+    /// <summary>
+    /// The amount of damage an object can take before it is destroyed
+    /// </summary>
+    [SerializeField] protected int Strength = 10;
+    
+
+    protected void ObjectHit(int damageTaken)
     {
-        
+        if (this.IsDestroyable)
+        {
+            Strength -= damageTaken;
+        }
+
+        if (Strength <= 0)
+        {
+            DestroyObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DestroyObject()
     {
-        
+        Destroy(gameObject);
     }
+
+    
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
 }
